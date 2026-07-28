f39d3e516c93a822544ca0064a2c0c0839126f83
loucas monir <loucasmonir@gmail.com>
2026-07-28 23:35:43 +0300

    explicitly add allowed actions by cors

--- server/src/main.ts
+++ server/src/main.ts
@@ -36,6 +36,7 @@ async function bootstrap() {
   app.enableCors({
     origin: process.env.CORS_ORIGIN || 'http://localhost:5173',
     credentials: true,
+    methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE', 'OPTIONS'],
   });
 
   const config = new DocumentBuilder()
